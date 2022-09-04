899556369bbdfe8046a5076d3d40e9c559e5f57e
RifatShamsuddin <rifatshamsuddin@gmail.com>
2022-09-04 17:38:41 +0600

    Last
    last

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,6 +23,11 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
+    public function index()
+    {
+        return view('welcome');
+    }
+
     public function welcome()
     {
         return view('welcome');
